f114c979c330af43b7c7d93c45ff21ac9f4354de
Krishna V D S <krishnavds@virtusa.com>
2018-05-30 13:19:00 +0530

    changed

--- Assignments/Week2/DOM-Manipulation/js/app.js
+++ Assignments/Week2/DOM-Manipulation/js/app.js
@@ -4,18 +4,19 @@ function identifyById() {
 }
 
 function identifyByClass() {
-
+    console.log(document.getElementByClass('class1'));
 }
 
 function identifyByTag() {
-
+    console.log(document.getElementByTagName('p'));
 }
 
 function changeBackground() {
-
+    document.getElementById('block1').style.backgroundColor="blue";
 }
 
 function increaseFont() {
+    document.getElementById()
 }
 
 function changeFontColor() {
